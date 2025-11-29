7f44f25918958878fe782b11eb40feae0762e792
Mayya <abeillee@bk.ru>
2025-11-30 01:55:00 +0300

    refactor: add missed imports

--- src/favorites/dto/favorites-response.dto.ts
+++ src/favorites/dto/favorites-response.dto.ts
@@ -1,4 +1,7 @@
 import { IsArray } from 'class-validator';
+import { Album } from 'src/albums/entities/album.entity';
+import { Artist } from 'src/artists/entities/artist.entity';
+import { Track } from 'src/tracks/entities/trask.entity';
 
 export class FavoritesResponseDto {
   @IsArray()
