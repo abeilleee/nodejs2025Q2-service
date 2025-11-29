37fb344eb4563db66219cab4aa05062e3a3718f6
Mayya <abeillee@bk.ru>
2025-11-29 16:10:00 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+import { loadSwaggerDocument } from './utils/loadSwaggerDocument';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const document = await loadSwaggerDocument();
+
+  dotenv.config();
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT);
 }
+
 bootstrap();
