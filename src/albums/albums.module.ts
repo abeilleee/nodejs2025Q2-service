74504bdd4d9faafe9c62906ff28304f603fdcff7
Mayya <abeillee@bk.ru>
2025-11-29 23:28:42 +0300

    refactor: resolve сircular dependencies between modules

--- src/albums/albums.module.ts
+++ src/albums/albums.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { TracksModule } from 'src/tracks/tracks.modules';
+import { FavoritesModule } from 'src/favorites/favorites.module';
 import { AlbumsController } from './albums.controller';
 import { AlbumsService } from './albums.service';
 
 @Module({
+  imports: [forwardRef(() => TracksModule), forwardRef(() => FavoritesModule)],
   controllers: [AlbumsController],
   providers: [AlbumsService],
   exports: [AlbumsService],
--- src/artists/artists.module.ts
+++ src/artists/artists.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { AlbumsModule } from 'src/albums/albums.module';
+import { TracksModule } from 'src/tracks/tracks.modules';
+import { FavoritesModule } from 'src/favorites/favorites.module';
 import { ArtistsController } from './artists.controller';
 import { ArtistsService } from './artists.service';
 
 @Module({
+  imports: [
+    forwardRef(() => AlbumsModule),
+    forwardRef(() => TracksModule),
+    forwardRef(() => FavoritesModule),
+  ],
   controllers: [ArtistsController],
   providers: [ArtistsService],
   exports: [ArtistsService],
