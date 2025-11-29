921283296dd1e59fd30d189f37b20d6bff98e0df
Mayya <abeillee@bk.ru>
2025-11-30 01:13:59 +0300

    refactor: remove unused var

--- src/albums/albums.service.ts
+++ src/albums/albums.service.ts
@@ -1,9 +1,4 @@
-import {
-  forwardRef,
-  Inject,
-  Injectable,
-  NotFoundException,
-} from '@nestjs/common';
+import { forwardRef, Inject, Injectable } from '@nestjs/common';
 import { FavoritesService } from 'src/favorites/favorites.service';
 import { TracksService } from 'src/tracks/tracks.service';
 import { ERROR_MESSAGE } from 'src/constants';
