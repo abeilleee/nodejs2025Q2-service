27478de4f37f092c5a70c69ffdce5bd5a441b8b6
Mayya <abeillee@bk.ru>
2025-12-13 19:09:44 +0300

    feat: integrate custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
 import { loadSwaggerDocument } from './utils/loadSwaggerDocument';
-import { ValidationPipe } from '@nestjs/common';
+import { LoggingService } from './logging/logging.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const logger = new LoggingService();
+  const app = await NestFactory.create(AppModule, { logger });
   const document = await loadSwaggerDocument();
 
   app.useGlobalPipes(new ValidationPipe({}));
